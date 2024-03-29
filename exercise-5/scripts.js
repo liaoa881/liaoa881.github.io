a2c536215a88696f6fbf8cb2f3b64f4bc9901be1
liaoa881 <145478599+liaoa881@users.noreply.github.com>
2024-03-29 00:44:00 -0400

    Exercise 5
    Exercise 5 addition

---
+++ exercise-5/scripts.js
@@ -0,0 +1,56 @@
+let squareOne = document.getElementById("square1");
+let squareTwo = document.getElementById("square2");
+let squareThree = document.getElementById("square3");
+let squareFour = document.getElementById("square4");
+let squareFive = document.getElementById("square5");
+let squareSix = document.getElementById("square6");
+let squareSeven = document.getElementById("square7");
+let squareEight = document.getElementById("square8");
+let squareNine = document.getElementById("square9");
+let squareTen = document.getElementById("square10");
+let squareEleven = document.getElementById("square11");
+let squareTwelve = document.getElementById("square12");
+let squareThirteen = document.getElementById("square13");
+let squareFourteen = document.getElementById("square14");
+let squareFifteen = document.getElementById("square15");
+let squareSixteen = document.getElementById("square16");
+
+
+squareOne.style.transform = "rotate(" + Math.random() * 360 + "deg)";
+squareTwo.style.transform = "rotate(" + Math.random() * 360 + "deg)";
+squareThree.style.transform = "rotate(" + Math.random() * 360 + "deg)";
+squareFour.style.transform = "rotate(" + Math.random() * 360 + "deg)";
+squareFive.style.transform = "rotate(" + Math.random() * 360 + "deg)";
+squareSix.style.transform = "rotate(" + Math.random() * 360 + "deg)";
+squareSeven.style.transform = "rotate(" + Math.random() * 360 + "deg)";
+squareEight.style.transform = "rotate(" + Math.random() * 360 + "deg)";
+squareNine.style.transform = "rotate(" + Math.random() * 360 + "deg)";
+squareTen.style.transform = "rotate(" + Math.random() * 360 + "deg)";
+squareEleven.style.transform = "rotate(" + Math.random() * 360 + "deg)";
+squareTwelve.style.transform = "rotate(" + Math.random() * 360 + "deg)";
+squareThirteen.style.transform = "rotate(" + Math.random() * 360 + "deg)";
+squareFourteen.style.transform = "rotate(" + Math.random() * 360 + "deg)";
+squareFifteen.style.transform = "rotate(" + Math.random() * 360 + "deg)";
+squareSixteen.style.transform = "rotate(" + Math.random() * 360 + "deg)";
+
+
+squareOne.style.borderBottomRightRadius = Math.random() * 50 + "%";
+squareTwo.style.borderBottomRightRadius = Math.random() * 50 + "%";
+squareThree.style.borderBottomRightRadius = Math.random() * 50 + "%";
+squareFour.style.borderBottomRightRadius = Math.random() * 50 + "%";
+squareFive.style.borderBottomRightRadius = Math.random() * 50 + "%";
+squareSix.style.borderBottomRightRadius = Math.random() * 50 + "%";
+squareSeven.style.borderBottomRightRadius = Math.random() * 50 + "%";
+squareEight.style.borderBottomRightRadius = Math.random() * 50 + "%";
+squareNine.style.borderBottomRightRadius = Math.random() * 50 + "%";
+squareTen.style.borderBottomRightRadius = Math.random() * 50 + "%";
+squareEleven.style.borderBottomRightRadius = Math.random() * 50 + "%";
+squareTwelve.style.borderBottomRightRadius = Math.random() * 50 + "%";
+squareThirteen.style.borderBottomRightRadius = Math.random() * 50 + "%";
+squareFourteen.style.borderBottomRightRadius = Math.random() * 50 + "%";
+squareFifteen.style.borderBottomRightRadius = Math.random() * 50 + "%";
+squareSixteen.style.borderBottomRightRadius = Math.random() * 50 + "%";
+
+
+
+
